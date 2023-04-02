0b3438b9efa12019285c2970be335b15ef1ff16a
iRohitKodape <rohitlkodape111@gmail.com>
2023-04-02 23:07:41 +0530

    javascript folder added

---
+++ javascript/practice.js
@@ -0,0 +1,82 @@
+// const obj = {
+//   fname: "rohit",
+//   lang: "javaScript",
+// };
+
+// console.log(obj);
+// console.log(obj.fname);
+
+// const obj2 = {
+//   fname: "rohit",
+//   lang: "javaScript",
+//   frontend: {
+//     tailwind: true,
+//     bootStrap: true,
+//   },
+// };
+
+// console.log(obj2.frontend.tailwind)
+// console.log(obj2.frontend.bootStrap);
+
+// const obj3 = new Object()
+
+// obj3.book = "awareness";
+// obj3.ebook = "no-mind";
+
+// console.log(obj3)
+
+// const obj4 = Object.create(obj3);
+// console.log(obj4);
+// console.log(obj4.ebook);
+
+// console.log(Object.getPrototypeOf(obj4))
+
+// const obj5 = Object.create({});
+// Object.defineProperty(obj5, "framework", {
+// //   yes: true,
+//     enumerable:true,
+//     get: () => true,
+// });
+
+// console.log(obj5)
+// console.log(obj5.framework);
+
+// for (k in obj5) {
+//     console.log(k)
+// }
+
+// const obj6 = {
+//   comics: "marvel",
+//   hero: "",
+
+//   printPen: function () {
+//     this.hero = "spiderman";
+//     // console.log(this);
+//     return this;
+//   },
+//   printCom: () => {
+//     hero = "IronMan";
+//   },
+// };
+
+// console.log(obj6.hero)
+
+// const fobj = {
+//   foo: 1,
+// };
+
+// console.log(Object.propertyIsEnumerable.call(fobj, "foo"));
+
+const age = {
+  alice: 18,
+  bob: 20,
+};
+
+function hasPerson(name) {
+  return name in age;
+}
+
+let res = hasPerson("hasOwnProperty");
+console.log(res);
+
+
