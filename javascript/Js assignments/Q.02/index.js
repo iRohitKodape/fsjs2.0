e1bfebb64f50f77ec2a43c2c1fb32b1756a7729e
iRohitKodape <rohitlkodape111@gmail.com>
2023-04-05 21:31:23 +0530

    Question no.2 solved

---
+++ javascript/Js assignments/Q.02/index.js
@@ -0,0 +1,11 @@
+let fname = "Rohit";
+let lname = "Kodape";
+let maritalStatus = false;
+let country = "INDIA";
+let age = 27;
+
+console.log(`my firstName is ${fname}`);
+console.log(`my lastName is ${lname}`);
+console.log(`my maritalStatus is ${maritalStatus}`);
+console.log(`my country is ${country}`);
+console.log(`my age is ${age}`);
