43568ce6be3d7a481860fc67f0f3b048f5592016
iRohitKodape <rohitlkodape111@gmail.com>
2023-04-05 22:16:10 +0530

    Question added to 2 and 3

--- javascript/Js assignments/Q.03/index.js
+++ javascript/Js assignments/Q.03/index.js
@@ -1,3 +1,5 @@
+// 03. Declare a varibale and assign string value to it and change all the string characters to capital letters using toUpperCase() method.
+
 const str = "fullstack javascript 2.0";
 
 const changedStr = str.toUpperCase();
