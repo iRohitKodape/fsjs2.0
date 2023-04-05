21d734a721650818165b9620e5f1f3b7151a65ef
iRohitKodape <rohitlkodape111@gmail.com>
2023-04-05 21:24:33 +0530

    Question no.1 solved

---
+++ javascript/Js assignments/Q.01/variable.js
@@ -0,0 +1,13 @@
+//01. Create a variable.js file and declare variables and assign string, boolean, undefined and null data types, Display all the value with their data type.
+
+let num = 1234;
+let Str = "Hello world";
+let bool = true;
+let Undef;
+let nullVar = null;
+
+console.log(`The type of ${num} variable is ${typeof num}`);
+console.log(`The type of ${Str} variable is ${typeof Str}`);
+console.log(`The type of ${bool} variable is ${typeof bool}`);
+console.log(`The type of ${Undef} variable is ${typeof Undef}`);
+console.log(`The type of ${nullVar} variable is ${typeof nullVar}`);
