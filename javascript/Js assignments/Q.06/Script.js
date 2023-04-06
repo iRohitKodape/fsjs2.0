4f8a7e5acc60bf0d66d1b6ef04c6041b38d03bc1
iRohitKodape <rohitlkodape111@gmail.com>
2023-04-06 22:58:07 +0530

    Question 6 and 7 solved

---
+++ javascript/Js assignments/Q.06/Script.js
@@ -0,0 +1,7 @@
+// 06. 'Facebook, Google, Microsoft, Apple, IBM, Oracle, Amazon' split the string at the comma and change it to an array.
+
+
+const longStr = "Facebook, Google, Microsoft, Apple, IBM, Oracle, Amazon";
+
+const newStr = longStr.split(",");
+console.log(typeof newStr);
---
+++ javascript/Js assignments/Q.07/script.js
@@ -0,0 +1,6 @@
+// 07. Declare an array containing the multiple values and use lastIndexOf to determine the position of the first and last occurrence of a word "pw skills".
+
+let arr1 = ["pw skills", "ineuron", "youtube", "freecodecamp","pw skills"];
+
+let index = arr1.lastIndexOf("pw skills");
+console.log(index);
