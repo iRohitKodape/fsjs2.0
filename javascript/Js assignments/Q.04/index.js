ebabe768c1f2e58173575baf0699009abcd13d69
iRohitKodape <rohitlkodape111@gmail.com>
2023-04-06 21:01:14 +0530

    Question 4 and 5 solved

---
+++ javascript/Js assignments/Q.04/index.js
@@ -0,0 +1,8 @@
+// 04. Declare a varibale and assign string value to it and check if the string contains a word Script using includes() method.
+
+
+let strVar = "javaScript";
+
+const newStr =strVar.includes("Script");
+
+console.log(newStr);
---
+++ javascript/Js assignments/Q.05/index.js
@@ -0,0 +1,10 @@
+
+// 05. Declare a varibale and assign string value to it and then split it into an array using split() method
+
+
+
+const orgStr = "javaScript";
+
+const splitStr = orgStr.split('');
+
+console.log(splitStr)
