5548749b78fcfb0fed442d47ec3413b239abf708
iRohitKodape <rohitlkodape111@gmail.com>
2023-04-09 22:07:05 +0530

    notes added

--- javascript/Js assignments/Q.17/script.js
+++ javascript/Js assignments/Q.17/script.js
@@ -1,31 +1,41 @@
 // 17. Write a program which tells the number of days in a month.
 
 // let input = prompt("enter Month name");
-let input = "January";
+const input = "February";
+const days31 = 31;
+const days30 = 30;
+const days28 = 28;
 
-const monthObj = {
-  January: "31",
-  February: "28",
-  March: "31",
-  April: "30",
-  May: "31",
-  June: "30",
-  July: "31",
-  August: "31",
-  September: "30",
-  October: "31",
-  November: "30",
-  December: "31",
-};
-function checkDay(inp, months) {
-  for (x in months) {
-    if (x === inp) {
-      return months.inp;
+const monthArr31 = [
+  "January",
+  "March",
+  "May",
+  "July",
+  "August",
+  "October",
+  "December",
+];
+
+const monthArr30 = ["April", "June", "September", "November"];
+
+const date28 = ["February"];
+
+function checkForDays(monthArr, inp, days) {
+  for (const element of monthArr) {
+    if (element === inp) {
+      console.log(`${element} month has ${days} days`);
     } else {
-      return "";
+      continue;
     }
   }
 }
 
-let res = checkDay(input, monthObj);
-console.log(res);
+if (monthArr31.includes(input)) {
+  checkForDays(monthArr31, input, days31);
+} else if (monthArr30.includes(input)) {
+  checkForDays(monthArr30, input, days30);
+} else if (date28.includes(input)) {
+  checkForDays(date28, input, days28);
+} else {
+  console.log(`check your input`);
+}
