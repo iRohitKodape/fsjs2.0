161baf76321362c773522609e1b1b73d17285113
iRohitKodape <rohitlkodape111@gmail.com>
2023-04-10 23:15:32 +0530

    Question 18 solved

--- javascript/Js assignments/Q.18/script.js
+++ javascript/Js assignments/Q.18/script.js
@@ -1,2 +1,15 @@
 // 18. Write a program which tells the number of days in a month, now consider leap year.
 
+let year = prompt("enter year");
+let month = prompt("enter date");
+
+const dateArr = [];
+dateArr.push(year);
+dateArr.push(month);
+// console.log(dateArr);
+function checkDays(year, month) {
+  return new Date(year, month, 0).getDate();
+}
+
+const res = checkDays(...dateArr);
+console.log(res);
