2fc31f4f3ec0ef4292d98d2d7173d6f8a4ce96ae
iRohitKodape <rohitlkodape111@gmail.com>
2023-04-19 13:26:14 +0530

    Project 50% completed

--- React/Assignment-projects/Todo-app/src/App.jsx
+++ React/Assignment-projects/Todo-app/src/App.jsx
@@ -1,40 +1,121 @@
 import React, { useState } from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faCircleCheck,
+  faPen,
+  faTrash,
+} from "@fortawesome/free-solid-svg-icons";
 import "./App.css";
-import TodoInput from "./Components/TodoInput";
-import TodoList from "./Components/TodoList";
 
 const App = () => {
-  const [listTodo, setListTodo] = useState([]);
+  const [todo, setTodo] = useState([
+    {
+      id: 1,
+      title: "Task1",
+      status: false,
+    },
+    { id: 2, title: "Task2", status: false },
+  ]);
 
-  let addList = (inpText) => {
-    if (inpText !== "") {
-      setListTodo([...listTodo, inpText]);
+  // temp state
+  const [newTask, setNewTask] = useState("");
+  const [updateData, setUpdateData] = useState("");
+
+  // Add task
+
+  const addTask = () => {
+    if (newTask) {
+      let num = todo.length + 1;
+      let newEntry = { id: num, title: newTask, status: false };
+      setTodo([...todo, newEntry]);
+      setNewTask("");
     }
   };
+  // delete task
+  const deleteTask = (id) => {};
 
-  const deleteListItem = (key) => {
-    let newTodoList = [...listTodo];
-    newTodoList.splice(key, 1);
-    setListTodo([...newTodoList]);
-  };
+  // mark the task
+  const markDone = (id) => {};
+
+  // cancel the update
+  const cancelUpdate = (id) => {};
+
+  // change task
+  const changeTask = (e) => {};
+
+  // update task
+  const updateTask = () => {};
+
   return (
-    <section className="main-container">
-      <div className="center-container">
-        <TodoInput addList={addList} />
-        <h1 className="app-heading">TODO</h1>
-        <hr />
-        {listTodo.map((listItem, index) => {
-          return (
-            <TodoList
-              key={index}
-              index={index}
-              item={listItem}
-              deleteListItem={deleteListItem}
+    <>
+      <div className="container">
+        <br />
+        <br />
+        <h2>To DO List App</h2>
+        <br />
+        <br />
+
+        {/* update task */}
+        <div className="row">
+          <div className="col">
+            <input type="text" className="form-control form-control-lg" />
+          </div>
+          <div className="col-auto">
+            <button className="btn btn-lg btn-success mr-20">update</button>
+            <button className="btn btn-lg btn-warning ">Cancel</button>
+          </div>
+        </div>
+        <br />
+        {/* add task */}
+        <div className="row">
+          <div className="col">
+            <input
+              type="text"
+              className="form-control form-control-lg"
+              value={newTask}
+              onChange={(e) => setNewTask(e.target.value)}
             />
-          );
-        })}
+          </div>
+          <div className="col-auto">
+            <button className="btn btn-lg btn-success" onClick={addTask}>
+              Add todo
+            </button>
+          </div>
+        </div>
+        <br />
+        {/* Display todos */}
+
+        {todo && todo.length ? "" : "no tasks..."}
+
+        {/* iterate over task */}
+        {todo &&
+          todo.sort().map((task, index) => {
+            return (
+              <React.Fragment key={task.id}>
+                <div className="col taskBg">
+                  <div className={task.status ? "done" : ""}>
+                    <span className="taskNumber">{index + 1}</span>
+                    <span className="taskText">{task.title}</span>
+                  </div>
+                  <div className="iconsWrap">
+                    <span title="completed/not completed">
+                      <FontAwesomeIcon icon={faCircleCheck} />
+                    </span>
+
+                    <span title="Edit">
+                      <FontAwesomeIcon icon={faPen} />
+                    </span>
+                    <span title="Delete">
+                      <FontAwesomeIcon icon={faTrash} />
+                    </span>
+                  </div>
+                </div>
+              </React.Fragment>
+            );
+          })}
       </div>
-    </section>
+    </>
   );
 };
 
