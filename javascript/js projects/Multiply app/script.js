b9697bb010887974a57ab5711ea23431611decb9
iRohitKodape <rohitlkodape111@gmail.com>
2023-04-21 23:59:38 +0530

    project completed

---
+++ javascript/js projects/Multiply app/script.js
@@ -0,0 +1,35 @@
+const num1 = Math.ceil(Math.random() * 10);
+const num2 = Math.ceil(Math.random() * 10);
+
+const questionEl = document.getElementById("question");
+const inputEl = document.getElementById("input");
+const formEl = document.getElementById("form");
+const scoreEl = document.getElementById("score");
+
+questionEl.innerText = `What is ${num1} multiply by ${num2}`;
+
+
+const correctAns = num1 * num2;
+
+let score = JSON.parse(localStorage.getItem("score"));
+scoreEl.innerText = `score:${score}`;
+
+if (!score) {
+  score = 0;
+}
+
+
+formEl.addEventListener("submit", (e) => {
+  const userAns = +inputEl.value;
+  if (userAns === correctAns) {
+    score++;
+    updateLocalStorage();
+  } else {
+    score--;
+    updateLocalStorage();
+  }
+});
+
+function updateLocalStorage() {
+  localStorage.setItem("score", JSON.stringify(score));
+}
