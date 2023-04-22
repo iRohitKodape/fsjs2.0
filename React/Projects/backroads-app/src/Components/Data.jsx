3b54535144e456e2d3b4fe5f71027a0897448130
iRohitKodape <rohitlkodape111@gmail.com>
2023-04-22 23:48:29 +0530

    Backroad project added

---
+++ React/Projects/backroads-app/src/Components/Data.jsx
@@ -0,0 +1,16 @@
+export const pageLinks = [
+  { id: 1, href: "#Home", text: "Home" },
+  { id: 2, href: "#About", text: "About" },
+  { id: 3, href: "#Services", text: "Services" },
+  { id: 4, href: "#Tours", text: "Tours" },
+];
+
+export const socialLinks = [
+  { id: 1, href: "https://www.twitter.com", icon: "fab fa-twitter" },
+  { id: 2, href: "https://www.facebook.com", icon: " fab fa-facebook" },
+  {
+    id: 3,
+    href: "https://www.squarespace.com",
+    icon: "fab fa-squarespace",
+  },
+];
