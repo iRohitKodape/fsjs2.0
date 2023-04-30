90a1827c8e371a05267a06a6f1facbcb8aefd402
iRohitKodape <rohitlkodape111@gmail.com>
2023-04-30 23:08:29 +0530

    todo app completed

--- React/Assignment-projects/todo-app/src/App.jsx
+++ React/Assignment-projects/todo-app/src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import TodoList from "./Components/TodoList";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faCirclePlus } from "@fortawesome/free-solid-svg-icons";
@@ -6,6 +6,22 @@ import { faCirclePlus } from "@fortawesome/free-solid-svg-icons";
 function App() {
   const [todos, setTodos] = useState([]);
   const [todo, setTodo] = useState("");
+  const [editingText, setEditingText] = useState("");
+  const [todoEditing, setTodoEditing] = useState(null);
+
+  useEffect(() => {
+    const temp = localStorage.getItem("todos");
+    const loadedTodos = JSON.parse(temp);
+
+    if (loadedTodos) {
+      setTodos(loadedTodos);
+    }
+  }, []);
+
+  useEffect(() => {
+    const temp = JSON.stringify(todos);
+    localStorage.setItem("todos", temp);
+  }, [todos]);
 
   function handleSubmit(e) {
     e.preventDefault();
@@ -19,11 +35,32 @@ function App() {
   }
 
   const deleteItem = (id) => {
-    const newTodos = todos.filter((t) => t.id !== id);
+    const newTodos = [...todos].filter((t) => t.id !== id);
     setTodos(newTodos);
   };
 
-  console.log(todos);
+  const completedTask = (id) => {
+    const completed = [...todos].map((todo) => {
+      if (todo.id == id) {
+        todo.completed = !todo.completed;
+      }
+      return todo;
+    });
+    setTodos(completed);
+  };
+
+  const editTodo = (id) => {
+    const updated = [...todos].map((todo) => {
+      if (todo.id === id) {
+        todo.txt = editingText;
+      }
+      return todo;
+    });
+    setTodos(updated);
+    setTodoEditing(null);
+    setEditingText("");
+  };
+
   return (
     <>
       <div className="container">
@@ -36,11 +73,23 @@ function App() {
             onChange={(e) => setTodo(e.target.value)}
           />
           <button type="submit">
-            <FontAwesomeIcon icon={faCirclePlus} size="xl"/>
+            <FontAwesomeIcon icon={faCirclePlus} size="xl" />
           </button>
         </form>
         {todos.map((t) => {
-          return <TodoList t={t} key={t.id} deleteItem={deleteItem} />;
+          return (
+            <TodoList
+              t={t}
+              key={t.id}
+              deleteItem={deleteItem}
+              completedTask={completedTask}
+              editingText={editingText}
+              setEditingText={setEditingText}
+              todoEditing={todoEditing}
+              setTodoEditing={setTodoEditing}
+              editTodo={editTodo}
+            />
+          );
         })}
       </div>
     </>
--- React/Assignment-projects/todo-app/src/Components/TodoList.jsx
+++ React/Assignment-projects/todo-app/src/Components/TodoList.jsx
@@ -1,20 +1,63 @@
-import React from "react";
+import React, { useState } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faTrash, faUser, faUserPen } from "@fortawesome/free-solid-svg-icons";
+import {
+  faEdit,
+  faTrash,
+  faUser,
+  faUserPen,
+} from "@fortawesome/free-solid-svg-icons";
 
-const TodoList = ({ t, deleteItem }) => {
+const TodoList = ({
+  t,
+  deleteItem,
+  completedTask,
+  todoEditing,
+  editingText,
+  setTodoEditing,
+  setEditingText,
+  editTodo,
+}) => {
   return (
     <div className="todolist">
       <div className="upper">
-        <input type="checkbox" value={t.completed} />
-        <p>{t.text}</p>
+        <input
+          type="checkbox"
+          onChange={() => completedTask(t.id)}
+          checked={t.completed}
+        />
+        {todoEditing === t.id ? (
+          <>
+            <input
+              type="text"
+              onChange={(e) => setEditingText(e.target.value)}
+              value={editingText}
+            />
+          </>
+        ) : (
+          <p>{t.text}</p>
+        )}
       </div>
       <div className="lower">
-        <FontAwesomeIcon icon={faUserPen} size="l"/>
+        {todoEditing == t.id ? (
+          <FontAwesomeIcon
+            icon={faEdit}
+            size="l"
+            onClick={() => editTodo(t.id)}
+            title="update"
+          />
+        ) : (
+          <FontAwesomeIcon
+            icon={faUserPen}
+            size="l"
+            onClick={() => setTodoEditing(t.id)}
+            title="edit"
+          />
+        )}
         <FontAwesomeIcon
           icon={faTrash}
           size="l"
           onClick={() => deleteItem(t.id)}
+          title="delete"
         />
       </div>
     </div>
