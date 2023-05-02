956ae9e248f75b63d23d97741deb53a253ef4afd
iRohitKodape <rohitlkodape111@gmail.com>
2023-05-02 20:15:53 +0530

    Reviews project started

---
+++ React/Projects/Reviews app/src/App.jsx
@@ -0,0 +1,8 @@
+import people from "./data";
+import React from "react";
+const App = () => {
+  const [index, setIndex] = React.useState(0);
+  
+  return <h2>Reviews Starter</h2>;
+};
+export default App;
