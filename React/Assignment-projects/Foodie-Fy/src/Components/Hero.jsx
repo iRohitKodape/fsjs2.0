e5bf2cc7e721f85ba958ba4e17ba68d96858ad6e
iRohitKodape <rohitlkodape111@gmail.com>
2023-05-08 22:14:29 +0530

    Header and nav section added

--- React/Assignment-projects/Foodie-Fy/src/Components/Hero.jsx
+++ React/Assignment-projects/Foodie-Fy/src/Components/Hero.jsx
@@ -3,11 +3,17 @@ import { FaSearch } from "react-icons/fa";
 const Hero = () => {
   return (
     <section className="hero">
-      <form action="">
-        <input type="text" placeholder="Search recipes here" />
-        <button type="submit">
-          <FaSearch size="24px" />
-        </button>
+      <form>
+        <section className="upper-form">
+          <input type="text" placeholder="Search recipes here" />
+          <button type="submit">
+            <FaSearch size="20px" color="white" />
+          </button>
+        </section>
+        <section className="hero-txt">
+          <p>Which are your Favourites</p>
+          <p>Experience Deliciously with us </p>
+        </section>
       </form>
     </section>
   );
