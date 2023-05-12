3f12e6d7ac0a8fa2ad9dfc2f6e062d994b1f0071
iRohitKodape <rohitlkodape111@gmail.com>
2023-05-12 21:04:18 +0530

    Categories on tabs added

--- React/Assignment-projects/Foodie-Fy/src/Api.jsx
+++ React/Assignment-projects/Foodie-Fy/src/Api.jsx
@@ -13,3 +13,29 @@ export const fetchDetails = async (query) => {
     console.error(error);
   }
 };
+
+export const fetchByCategory = async (category) => {
+  try {
+    const response = await axios.get(
+      `https://www.themealdb.com/api/json/v1/1/filter.php?c=${category}`
+    );
+    const { meals } = response.data;
+    console.log(meals);
+    return meals;
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export const fetchById = async (id) => {
+  try {
+    const response = await axios.get(
+      `https://www.themealdb.com/api/json/v1/1/lookup.php?i=${id}`
+    );
+    const mealObj = response.data.meals[0];
+    console.log(mealObj);
+    return mealObj;
+  } catch (error) {
+    console.log(error);
+  }
+};
--- React/Assignment-projects/Foodie-Fy/src/Components/Tabs.jsx
+++ React/Assignment-projects/Foodie-Fy/src/Components/Tabs.jsx
@@ -1,56 +1,112 @@
-import React from "react";
-import { CiPizza } from "react-icons/ci";
-import { GiFruitBowl, GiNoodles, GiCheckMark } from "react-icons/gi";
-import { MdOutlineIcecream } from "react-icons/md";
+import { useState, useEffect } from "react";
+
+import {
+  GiFruitBowl,
+  GiAnglerFish,
+  GiCheckMark,
+  GiThreeLeaves,
+} from "react-icons/gi";
+import { MdFreeBreakfast } from "react-icons/md";
+import { fetchByCategory, fetchById } from "../Api";
 
 function Tabs() {
+  const [active, setActive] = useState("Veg");
+  const [tabData, setTabData] = useState("");
+  const [tabLabel, setTabLabel] = useState([
+    {
+      name: "Veg",
+      icons: <GiThreeLeaves />,
+      id: "52864",
+    },
+    {
+      name: "SeaFood",
+      icons: <GiAnglerFish />,
+      id: "52959",
+    },
+    {
+      name: "Dessert",
+      icons: <GiFruitBowl />,
+      id: "53049",
+    },
+    {
+      name: "Breakfast",
+      icons: <MdFreeBreakfast />,
+      id: "52957",
+    },
+  ]);
+
+  const handleCLick = (name, id) => {
+    setActive(name);
+    fetchByCategory(name).then((res) => {
+      setTabData(res);
+    });
+  };
+
+  useEffect(() => {
+    fetchById(tabLabel[0].id).then((res) => {
+      setTabData(res);
+      console.log(res);
+    });
+  }, []);
+
   return (
     <div className="container">
       <h1 className="recipeHeading">What would you like to have!</h1>
       <div className="tabs">
-        <div className="tablist active">
-          <CiPizza />
-          <span>Pizza</span>
-        </div>
-        <div className="tablist">
-          <GiNoodles />
-          <span>Noodles</span>
-        </div>
-        <div className="tablist">
-          <GiFruitBowl />
-          <span>Desert</span>
-        </div>
-        <div className="tablist">
-          <MdOutlineIcecream />
-          <span>ice cream</span>
-        </div>
+        {tabLabel.map((item, index) => {
+          return (
+            <div
+              className={`tablist ${active === item.name ? "active" : ""}`}
+              key={index}
+              onClick={() => handleCLick(item.name, item.id)}
+            >
+              {item.icons}
+              <span>{item.name}</span>
+            </div>
+          );
+        })}
       </div>
       <div className="recipe_banner">
-        <div className="left-col">
-          <span className="badge">Italian</span>
-          <h1>White Pizza</h1>
-          <p>
-            <strong>Recipe by:</strong>
-            <small>Food52</small>
-          </p>
-          <h3>Ingredients</h3>
-          <div className="ingredients">
-            <ul>
-              <li>
-                <GiCheckMark size="18px" color="#6fcb9f" />
-                &nbsp;<span>Fresh ground pepper</span>
-              </li>
-            </ul>
-          </div>
-        </div>
-        <div className="right-col">
-          <div className="image-wrapper">
-            <img
-              src="https://images.unsplash.com/photo-1607532941433-304659e8198a?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1378&q=80"
-              alt=""
-            />
-          </div>
-        </div>
+        {tabData !== "" && (
+          <>
+            <div className="left-col">
+              <span className="badge">{tabData.strArea}</span>
+              <h1>{tabData.strMeal}</h1>
+              <p>
+                <strong>Recipe by:</strong>
+                <small>Foodie-fy</small>
+              </p>
+              <h3>Ingredients</h3>
+              <div className="ingredients">
+                <ul>
+                  <li>
+                    <GiCheckMark size="18px" color="#6fcb9f" />
+                    &nbsp;
+                    <span>{tabData.strIngredient1}</span>
+                  </li>
+                  <li>
+                    <GiCheckMark size="18px" color="#6fcb9f" />
+                    &nbsp;
+                    <span>{tabData.strIngredient2}</span>
+                  </li>
+                  <li>
+                    <GiCheckMark size="18px" color="#6fcb9f" />
+                    &nbsp;
+                    <span>{tabData.strIngredient3}</span>
+                  </li>
+                </ul>
+              </div>
+            </div>
+            <div className="right-col">
+              <div className="image-wrapper">
+                <img
+                  src="https://images.unsplash.com/photo-1607532941433-304659e8198a?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1378&q=80"
+                  alt=""
+                />
+              </div>
+            </div>
+          </>
+        )}
       </div>
     </div>
   );
