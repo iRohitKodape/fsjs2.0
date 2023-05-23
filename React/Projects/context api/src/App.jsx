0b5bb44fce101ce10ce15e65fb8e0302ea287a55
iRohitKodape <rohitlkodape111@gmail.com>
2023-05-23 13:26:11 +0530

    new context api completed

--- React/Projects/context api/src/App.jsx
+++ React/Projects/context api/src/App.jsx
@@ -1,47 +1,17 @@
-import Context from "./Context/Context";
-import Provider from "./Provider";
+import { useState } from "react";
+import ThemeContext from "./Context/Context";
+import Header from "./Components/Header";
+import HeroSection from "./Components/HeroSection";
 
-const Agents = () => {
-  return <AgentOne />;
-};
-
-const AgentOne = () => {
-  return <AgentTwo />;
-};
-
-const AgentTwo = () => {
-  return <AgentVinod />;
-};
-
-const AgentVinod = () => {
+const App = () => {
+  const themeHook = useState("light");
   return (
-    <Context.Consumer>
-      {(context) => (
-        <>
-          <h3>Agent Info:{context.data.agentId}</h3>
-          <p>Mission Name :{context.data.mName}</p>
-          <p>
-            Mission Name :{context.data.accept ? "ACCEPTED" : "NOT ACCEPTED"}
-          </p>
-
-          <button onClick={context.isAccepted}>ACCEPT the mission</button>
-        </>
-      )}
-    </Context.Consumer>
-  );
-};
-
-function App() {
-  return (
-    <>
-      <div>
-        <h1>hello react</h1>
-        <Provider>
-          <Agents />
-        </Provider>
+    <ThemeContext.Provider value={themeHook}>
+      <div className="">
+        <Header />
+        <HeroSection />
       </div>
-    </>
+    </ThemeContext.Provider>
   );
-}
-
+};
 export default App;
---
+++ React/Projects/context api/src/Components/Header.jsx
@@ -0,0 +1,11 @@
+import ThemeToggler from "./ThemeToggler";
+
+const Header = () => {
+  return (
+    <header>
+      <h1>Theme Toggler</h1>
+      <ThemeToggler />
+    </header>
+  );
+};
+export default Header;
