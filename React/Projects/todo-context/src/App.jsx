9505f59079f2afcb4bb429b6f8bdca4decbf0dde
iRohitKodape <rohitlkodape111@gmail.com>
2023-06-01 12:33:09 +0530

    Local storage used app completed

--- React/Projects/todo-context/src/App.jsx
+++ React/Projects/todo-context/src/App.jsx
@@ -1,10 +1,39 @@
+import { useEffect, useState } from "react";
+import { Container } from "reactstrap";
+import "bootstrap/dist/css/bootstrap.css";
+// import "./App.css";
 import TodoForm from "./Components/TodoForm";
+import Todos from "./Components/Todos";
 
 const App = () => {
+  const [todos, setTodos] = useState([]);
+
+  useEffect(() => {
+    const localTodos = localStorage.getItem("todos");
+    console.log({ localStorage });
+    if (localTodos) {
+      setTodos(JSON.parse(localTodos));
+    }
+  }, []);
+
+  const addTodos = async (todo) => {
+    setTodos([...todos, todo]);
+  };
+
+  useEffect(() => {
+    localStorage.setItem("todos", JSON.stringify(todos));
+  }, [todos]);
+
+  const markComplete = (id) => {
+    setTodos(todos.filter((todo) => todo.id !== id));
+  };
+
   return (
-    <>
-      <TodoForm />
-    </>
+    <Container fluid>
+      <h1>Todo with Local Storage</h1>
+      <Todos todos={todos} markComplete={markComplete} />
+      <TodoForm addTodos={addTodos} />
+    </Container>
   );
 };
 
