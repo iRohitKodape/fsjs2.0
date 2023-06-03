d0e0ff1df22a0e66c18830f0f683bb66e3ff247a
iRohitKodape <rohitlkodape111@gmail.com>
2023-06-03 17:30:12 +0530

    Random user app started

---
+++ React/Projects/Random user app/src/App.jsx
@@ -0,0 +1,13 @@
+import { useState, useEffect } from "react";
+import { Container, Row, Col } from "reactstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
+import axios from "axios";
+
+function App() {
+  const [details, setDetails] = useState({});
+
+  return <></>;
+}
+
+export default App;
